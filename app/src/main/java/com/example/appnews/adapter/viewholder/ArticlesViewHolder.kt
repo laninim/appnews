a4af85babdde9a894cb9741f3d670da3138421a6
Mauro Lanini <mallettomachico@gmail.com>
2023-02-03 14:22:01 +0100

    Refactor news fragments

--- app/src/main/java/com/example/appnews/adapter/viewholder/ArticlesViewHolder.kt
+++ app/src/main/java/com/example/appnews/adapter/viewholder/ArticlesViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.appnews.adapter.viewholder
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import coil.load
 import com.example.appnews.R
@@ -13,12 +14,13 @@ class ArticlesViewHolder(val view : View) : ViewHolder(view) {
     val titleView = view.findViewById<TextView>(R.id.article_title)
     val descriptionView = view.findViewById<TextView>(R.id.article_description)
     val dataView = view.findViewById<TextView>(R.id.article_date)
-    val imageView = view.findViewById<ImageView>(R.id.article_image)
+    val constraintlayout = view.findViewById<ConstraintLayout>(R.id.mainlayout)
+    val imagearticle = view.findViewById<ImageView>(R.id.article_image)
 
     fun bindArticle(article : Article){
         titleView.text = article.title
-        descriptionView.text = article.description
+        descriptionView.text = article.content
         dataView.text = article.publishedAt
-        imageView.load(article.urlToImage)
+        imagearticle.load(article.urlToImage)
     }
 }
