34019f5653969acf6298bf1da912b0a363f2945f
Mauro Lanini <mallettomachico@gmail.com>
2023-02-04 09:24:39 +0100

    Implements navigation

--- app/src/main/java/com/example/appnews/MainActivity.kt
+++ app/src/main/java/com/example/appnews/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.appnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomNavigationController()
+    }
+
+
+    private fun setupBottomNavigationController(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.default_nav_host) as NavHostFragment
+        val navController = navHost.navController
+        val bottomNavigation = this@MainActivity.findViewById<BottomNavigationView>(R.id.bottomnavigation)
+        bottomNavigation.setupWithNavController(navController)
     }
 }
